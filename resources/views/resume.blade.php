f3c4dd205df2a3424e68d03a7b473111d6a9fe58
Ion Cojocaru <cojocaru.ion.88@gmail.com>
2020-07-17 17:42:54 +0300

    Changed resume page

--- resources/views/resume.blade.php
+++ resources/views/resume.blade.php
@@ -8,7 +8,13 @@
                 <p class="text-center"><strong>cojocaru.ion.88@gmail.com</strong><br />
                     <table class="table table-bordered">
                         <tr>
-                            <td><h3><u>Education</u></h3></td>
+                            <td><h3>Personal Summary</h3></td>
+                            <td>
+                                New challenges make one's life more interesting - this is my main belief. I enjoy taking on new challenges, solving interesting problems and getting a perfect result. I am new to software development, but it is my favorite hobby so far. I've been learning algorithms and solving different problems for more than 15 years. Now I want to evolve my hobby into a career. I am currently working on a small project as a freelancer, as well as doing other small apps for my portfolio. Right now I would like to join a company where others encourage me, respect me and help me develop myself as a person and as a developer, a company where my knowledge matters.
+                            </td>
+                        </tr>
+                        <tr>
+                            <td><h3>Education</h3></td>
                             <td>
                 <p><strong>Mihail Berezovschi Lyceum, Chisinau</strong> (bachelor's degree) – <em>1995 - 2007</em></p>
                 <p><strong>Technical University of Moldova</strong> (masters's degree) – <em>2007 - 2013</em></p>
@@ -16,43 +22,53 @@
                 </tr>
 
                 <tr>
-                    <td><h3><u>Work Experience</u></h3></td>
+                    <td><h3>Work Experience</h3></td>
                     <td>
                         <p><strong>TSO dispatcher – </strong><em><a href="http://moldelectrica.md/ro/">SE Moldelectrica</a><br />
                                 August 2011 – present</em></p>
                         <ul>
                             <li>Maintain power balance and inter-system energy exchange within operational hour.</li>
-                            <li>Disturbance handling and remedial action</li>
-                            <li>Enhance secure functioning of the power system</li>
+                            <li>Read charts, meters, and gauges to monitor voltage and electricity flows.</li>
+                            <li>Check equipment and indicators to detect evidence of operating problems.</li>
+                            <li>
+                                Respond to emergencies, such as transformer or transmission line failures, and route current around affected areas.</li>
+                            <li>Direct personnel engaged in controlling or operating distribution equipment or machinery.</li>
+                            <li>Implement energy schedules, including real-time transmission reservations or schedules.</li>
+                            <li>Track conditions that could affect power needs, such as changes in the weather, and adjust equipment to meet any anticipated changes.</li>
+                            <li>Disturbance handling and remedial action.</li>
+                            <li>Enhance secure functioning of the power system.</li>
+                        </ul>
+                        <p><strong>Full-stack developer – </strong><em>Freelance<br />
+                                December 2019 – present</em></p>
+                        <ul>
+                            <li>Program, test and debug web applications.</li>
+                            <li>Design, develop, test and deploy web applications.</li>
+                            <li>Code pages, files, test, debug and deploy.</li>
+                            <li>Assist and support in the upkeep and maintenance of web sites.</li>
+                            <li>                                Fix bugs, troubleshoot and resolve problems.</li>
                         </ul>
                     </td>
                 </tr>
 
                 <tr>
-                    <td><h3><u>Achievements</u></h3></td>
+                    <td><h3>Technical Skills</h3></td>
+                    <td>
+                        PHP, Python, JavaScript, SQL, HTML5, CSS3, MATLAB, Windows, GitHub, Apache, PgAdmin 4, PhpMyAdmin, Bootstrap, Microsoft Office, Chrome Developer Tools
+                    </td>
+
+                </tr>
+
+                <tr>
+                    <td><h3>Completed courses</h3></td>
                     <td>
                         <p>
-                            <strong>TUM Science Conference – </strong>2008, 2011<br />
-                            <strong>TUM Foreign Languages Conference – </strong>2008, 2009, 2010<br />
-                            <strong>Best student of the year – </strong>2008-2009<br />
-                            <strong>Republic of Moldova Government Scholar – </strong>2009-2010<br />
-                            <strong>Victoriabank Scholar – </strong>2010-2011<br />
-                            <strong>Moldcell Scholar – </strong>2010-2011<br />
+                            <strong>CS50x (Harvard) – </strong>Mar 2020<br />
+                            <strong>PHP Laravel for beginners (Udemy) – </strong>Mar 2020<br />
+                            <strong>Symfony level 1 (SymfonyCasts) – </strong>Mar 2020<br />
                         </p>
                     </td>
                 </tr>
-                <tr>
-                    <td><h3><u>Technical Skills</u></h3></td>
-                    <td>
-                        <ul>
-                            <li>Windows 10/8/7, iOS, some Linux (Ubuntu)</li>
-                            <li>basic C, basic Python, basic HTML/CSS and JavaScript</li>
-                            <li>Autocad, MS Visio</li>
-                            <li>Mathcad and MatLab</li>
-                        </ul>
-                    </td>
 
-                </tr>
                 </table>
             </div>
         </div>
